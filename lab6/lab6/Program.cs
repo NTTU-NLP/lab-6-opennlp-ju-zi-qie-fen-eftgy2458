bca4801675497681485bb8cf64d48ce43782f21c
eftgy2458 <eftgy2458@yahoo.com.tw>
2017-04-09 21:54:27 +0800

    lab6

---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using opennlp.tools.sentdetect;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"..\..\Data\result.txt");
+            StreamReader sr = new StreamReader(@"..\..\Data\data.txt");
+            while (sr.Peek() != -1)
+            {
+                string line = sr.ReadLine();
+                java.io.InputStream modelIn = new java.io.FileInputStream("en-sent.bin");
+                SentenceModel smodel = new SentenceModel(modelIn);
+                SentenceDetector detector = new SentenceDetectorME(smodel);
+                string[] sents = detector.sentDetect(line);
+                foreach (var sent in sents)
+                {
+                    sw.WriteLine(sent);
+                    sw.WriteLine();
+                }
+                sw.Flush();
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
